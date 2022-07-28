f7c7c90ec62fc6fedb1c67b29d1a4958b09586e1
SergioGodina <a1121250032@utch.edu.mx>
2022-07-27 22:21:35 -0600

    temp ahora recibe Strings en vez de numeros: revertido

--- models/temp.js
+++ models/temp.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const temp = new mongoose.Schema({
     datetime: {
         required: true,
-        type: String
+        type: Number
     },
     current_temperature: {
         required: true,
-        type: String
+        type: Number
     },
     current_humidity: {
         required: true,
-        type: String
+        type: Number
     }
 })
 
